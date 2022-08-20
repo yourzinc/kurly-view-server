0cc9f4048b60291c927f674c8569ef2801fe72e6
yourzinc <your_zinc@ewhain.net>
2022-08-20 12:45:42 +0900

    chore: project initialize

---
+++ src/main/java/com/kurly/kurlyview/KurlyviewApplication.java
@@ -0,0 +1,13 @@
+package com.kurly.kurlyview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurly/kurlyview/KurlyviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurly.kurlyview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
