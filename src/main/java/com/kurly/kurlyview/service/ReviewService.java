d923fdc620b456a4bd33c37abdbdbeb2b1eff2d2
yourzinc <your_zinc@ewhain.net>
2022-08-24 00:21:42 +0900

    feat: 상품 주간 평균 별점 API / 신선식품 O

--- src/main/java/com/kurly/kurlyview/service/ReviewService.java
+++ src/main/java/com/kurly/kurlyview/service/ReviewService.java
@@ -114,6 +114,9 @@ public class ReviewService {
                 .build();
     }
 
+    /**
+     * 상품 리뷰 월간 평점
+     */
     public ReviewRateResponseDto findMontlyRate(String productId) {
 
         int product_type = productRepository.findById(productId).get().getType();
@@ -126,6 +129,23 @@ public class ReviewService {
         }
         return ReviewRateResponseDto.builder().build();
     }
+
+    /**
+     * 상품 리뷰 주간 평점
+     */
+    public ReviewRateResponseDto findWeeklyRate(String productId) {
+
+        int product_type = productRepository.findById(productId).get().getType();
+
+        if (product_type == 0) {
+            return nonFreshProductWeeklyRate(productId);
+        }
+        else if (product_type == 1) {
+            return freshProductWeeklyRate(productId);
+        }
+        return ReviewRateResponseDto.builder().build();
+    }
+
 
     public ReviewRateResponseDto nonFreshProductMontlyRate(String productId)
     {
@@ -284,18 +304,6 @@ public class ReviewService {
                 .build();
     }
 
-    public ReviewRateResponseDto findWeeklyRate(String productId) {
-
-        int product_type = productRepository.findById(productId).get().getType();
-
-        if (product_type == 0) {
-            return nonFreshProductWeeklyRate(productId);
-        }
-        else if (product_type == 1) {
-            return nonFreshProductWeeklyRate(productId);
-        }
-        return ReviewRateResponseDto.builder().build();
-    }
 
     private ReviewRateResponseDto nonFreshProductWeeklyRate(String productId) {
         List<Rate> rating = new ArrayList<>();
@@ -316,16 +324,20 @@ public class ReviewService {
         reviewRepository.findAllByProductId(productId).stream()
                 .forEach(review -> {
                     LocalDateTime review_date = review.getDate();
-                    if (review_date.isBefore(m1.getDate().atStartOfDay()) || review_date.isEqual(m1.getDate().atStartOfDay())) {
+                    if (review_date.isAfter(m1.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m1.getDate().atStartOfDay().plusDays(1))) {
                         m1.setRate(m1.getRate() + review.getRating());
                         c1.getAndIncrement();
-                    } else if (review_date.isBefore(m2.getDate().atStartOfDay()) || review_date.isEqual(m2.getDate().atStartOfDay())) {
+                    } else if (review_date.isAfter(m2.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m2.getDate().atStartOfDay().plusDays(1))) {
                         m2.setRate(m2.getRate()+review.getRating());
                         c2.getAndIncrement();
-                    } else if (review_date.isBefore(m3.getDate().atStartOfDay()) || review_date.isEqual(m3.getDate().atStartOfDay())) {
+                    } else if (review_date.isAfter(m3.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m3.getDate().atStartOfDay().plusDays(1))) {
                         m3.setRate(m3.getRate()+review.getRating());
                         c3.getAndIncrement();
-                    } else if (review_date.isBefore(m4.getDate().atStartOfDay()) || review_date.isEqual(m4.getDate().atStartOfDay())) {
+                    } else if (review_date.isAfter(m4.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m4.getDate().atStartOfDay().plusDays(1))) {
                         m4.setRate(m4.getRate()+review.getRating());
                         c4.getAndIncrement();
                     }
@@ -341,6 +353,132 @@ public class ReviewService {
                 .rating(rating)
                 .build();
     }
-//    private ReviewRateResponseDto freshProductWeeklyRate(String productId) {
-//    }
+    private ReviewRateResponseDto freshProductWeeklyRate(String productId) {
+        List<Rate> rating = new ArrayList<>();
+        List<Rate> fresh_score = new ArrayList<>();
+        List<Rate> taste_score = new ArrayList<>();
+        List<Rate> delivery_score = new ArrayList<>();
+
+        // count
+        AtomicInteger c1 = new AtomicInteger();
+        AtomicInteger c2 = new AtomicInteger();
+        AtomicInteger c3 = new AtomicInteger();
+        AtomicInteger c4 = new AtomicInteger();
+
+        LocalDate now = LocalDate.now();
+
+        // rating
+        Rate m1 = new Rate(now, 0.0);
+        Rate m2 = new Rate(now.minusWeeks(1), 0.0);
+        Rate m3 = new Rate(now.minusWeeks(2), 0.0);
+        Rate m4 = new Rate(now.minusWeeks(3), 0.0);
+
+        // fresh score
+        Rate f1 = new Rate(now, 0.0);
+        Rate f2 = new Rate(now.minusWeeks(1), 0.0);
+        Rate f3 = new Rate(now.minusWeeks(2), 0.0);
+        Rate f4 = new Rate(now.minusWeeks(3), 0.0);
+
+        // taste score
+        Rate t1 = new Rate(now, 0.0);
+        Rate t2 = new Rate(now.minusWeeks(1), 0.0);
+        Rate t3 = new Rate(now.minusWeeks(2), 0.0);
+        Rate t4 = new Rate(now.minusWeeks(3), 0.0);
+
+        // delivery score
+        Rate d1 = new Rate(now, 0.0);
+        Rate d2 = new Rate(now.minusWeeks(1), 0.0);
+        Rate d3 = new Rate(now.minusWeeks(2), 0.0);
+        Rate d4 = new Rate(now.minusWeeks(3), 0.0);
+
+
+        reviewRepository.findAllByProductId(productId).stream()
+                .forEach(review -> {
+                    LocalDateTime review_date = review.getDate();
+                    if (review_date.isAfter(m1.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m1.getDate().atStartOfDay().plusDays(1))) {
+                        log.info("m1: "+m1.getDate().toString());
+                        log.info("review.date: "+ review_date.toLocalDate());
+                        m1.setRate(m1.getRate() + review.getRating());
+                        f1.setRate(f1.getRate()+ review.getFreshScore());
+                        t1.setRate(t1.getRate()+ review.getTasteScore());
+                        d1.setRate(d1.getRate()+ review.getDeliveryScore());
+                        c1.getAndIncrement();
+                    } else if (review_date.isAfter(m2.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m2.getDate().atStartOfDay().plusDays(1))) {
+                        log.info("m2: "+m1.getDate().toString());
+                        log.info("review.date: "+ review_date.toLocalDate());
+                        m2.setRate(m2.getRate()+review.getRating());
+                        f2.setRate(f2.getRate()+ review.getFreshScore());
+                        t2.setRate(t2.getRate()+ review.getTasteScore());
+                        d2.setRate(d2.getRate()+ review.getDeliveryScore());
+                        c2.getAndIncrement();
+                    } else if (review_date.isAfter(m3.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m3.getDate().atStartOfDay().plusDays(1))) {
+                        log.info("m3: "+m1.getDate().toString());
+                        log.info("review.date: "+ review_date.toLocalDate());
+                        m3.setRate(m3.getRate()+review.getRating());
+                        f3.setRate(f3.getRate()+ review.getFreshScore());
+                        t3.setRate(t3.getRate()+ review.getTasteScore());
+                        d3.setRate(d3.getRate()+ review.getDeliveryScore());
+                        c3.getAndIncrement();
+                    } else if (review_date.isAfter(m4.getDate().atStartOfDay().minusDays(7))
+                            & review_date.isBefore(m4.getDate().atStartOfDay().plusDays(1))) {
+
+                        log.info("m4: "+m1.getDate().toString());
+                        log.info("review.date: "+ review_date.toLocalDate());
+                        m4.setRate(m4.getRate()+review.getRating());
+                        f4.setRate(f4.getRate()+ review.getFreshScore());
+                        t4.setRate(t4.getRate()+ review.getTasteScore());
+                        d4.setRate(d4.getRate()+ review.getDeliveryScore());
+                        c4.getAndIncrement();
+                    }
+                });
+
+        if (c1.get() != 0){
+            m1.setRate(m1.getRate()/c1.get());
+            f1.setRate(f1.getRate()/c1.get());
+            t1.setRate(t1.getRate()/c1.get());
+            d1.setRate(d1.getRate()/c1.get());
+        }
+        if (c2.get() != 0){
+            m2.setRate(m2.getRate()/c2.get());
+            f2.setRate(f2.getRate()/c2.get());
+            t2.setRate(t2.getRate()/c2.get());
+            d2.setRate(d2.getRate()/c2.get());
+        }
+        if (c3.get() != 0) {
+            m3.setRate(m3.getRate()/c3.get());
+            f3.setRate(f3.getRate()/c3.get());
+            t3.setRate(t3.getRate()/c3.get());
+            d3.setRate(d3.getRate()/c3.get());
+        }
+        if (c4.get() != 0){
+            m4.setRate(m4.getRate()/c4.get());
+            f4.setRate(f4.getRate()/c4.get());
+            t4.setRate(t4.getRate()/c4.get());
+            d4.setRate(d4.getRate()/c4.get());
+        }
+
+        for (Rate m : new Rate[]{m4, m3, m2, m1})
+            rating.add(m);
+        for (Rate f : new Rate[]{f4, f3, f2, f1})
+            fresh_score.add(f);
+        for (Rate t :new Rate[]{t4, t3, t2, t1})
+            taste_score.add(t);
+        for (Rate d : new Rate[]{d4, d3, d2, d1})
+            delivery_score.add(d);
+
+        log.info(rating.toString());
+        log.info(fresh_score.toString());
+        log.info(taste_score.toString());
+        log.info(delivery_score.toString());
+
+        return ReviewRateResponseDto.builder()
+                .rating(rating)
+                .fresh_score(fresh_score)
+                .taste_score(taste_score)
+                .delivery_score(delivery_score)
+                .build();
+    }
 }
