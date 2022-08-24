0ad8a06ea2a090255df7f412573fc86e44341ab7
yourzinc <your_zinc@ewhain.net>
2022-08-24 14:15:38 +0900

    feat: Review sort by date

--- src/main/java/com/kurly/kurlyview/repository/ReviewRepository.java
+++ src/main/java/com/kurly/kurlyview/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.kurly.kurlyview.repository;
 
 import com.kurly.kurlyview.domain.product.Product;
 import com.kurly.kurlyview.domain.review.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,8 +14,9 @@ import java.util.Optional;
 public interface ReviewRepository extends MongoRepository<Review, String> {
     Optional<Review> findById(String id);
     List<Review> findByMemberId(@Param("member_id") String memberId);
+    List<Review> findByMemberId(@Param("member_id") String memberId, Pageable pageable);
     List<Review> findAllByProductId(@Param("product_id") String productId);
-
+    List<Review> findAllByProductId(@Param("product_id") String productId, Pageable pageable);
     List<Review> findAllByMemberIdAndRating(@Param("member_id") String memberId,
                                             @Param("rating") Integer rating);
 }
